b1dd9719431b7987d2cdee2c0d8c0aedb61475b3
G <zzmrlol@gmail.com>
2021-04-22 18:55:01 +0300

    Add date picker to form

--- application_forms/forms.py
+++ application_forms/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from django.contrib.admin import widgets
+from django.forms.widgets import SelectDateWidget
 
 from .models import ExhibitionApplication, QuizzesForStudentsApplication
 
@@ -9,12 +9,16 @@ class ExhibitionApplicationForm(ModelForm):
         model = ExhibitionApplication
         exclude = ['completed']
 
-    # def __init__(self, *args, **kwargs):
-    #     super(ExhibitionApplicationForm, self).__init__(*args, **kwargs)
-    #     self.fields['exhibition_date'].widget = widgets.AdminSplitDateTime()
+    def __init__(self, *args, **kwargs):
+        super(ExhibitionApplicationForm, self).__init__(*args, **kwargs)
+        self.fields['exhibition_date'].widget = SelectDateWidget()
 
 
 class QuizzesForm(ModelForm):
     class Meta:
         model = QuizzesForStudentsApplication
         exclude = ['completed']
+
+    def __init__(self, *args, **kwargs):
+        super(QuizzesForm, self).__init__(*args, **kwargs)
+        self.fields['exhibition_date'].widget = SelectDateWidget()
