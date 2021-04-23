20b0cf847296466e6d4059ca388dc43d9aea2a5a
G <zzmrlol@gmail.com>
2021-04-23 11:44:47 +0300

    Change exhibition_date\place to date\place

--- application_forms/models.py
+++ application_forms/models.py
@@ -16,17 +16,17 @@ class ExhibitionApplication(models.Model):
     surname = models.CharField(verbose_name='Фамилия', max_length=50)
     city = models.CharField(verbose_name='Город', max_length=50)
     contacts = models.CharField(verbose_name='ВК или email', max_length=100)
-    who_is_organize = models.CharField(verbose_name='Кто будет организатором выстаки?',
+    who_is_organize = models.CharField(verbose_name='Кто будет организатором выставки?',
                                        max_length=100,
                                        choices=ORGANIZER,
                                        default='Лично')
-    exhibition_date = models.DateField(verbose_name='Когда примерно хотите организовывать выставку?')
-    exhibition_place = models.CharField(verbose_name='Где хотите провести выставку?', max_length=100)
+    date = models.DateField(verbose_name='Когда примерно хотите организовывать выставку?')
+    place = models.CharField(verbose_name='Где хотите провести выставку?', max_length=100)
     completed = models.BooleanField(verbose_name='Проведена ли выставка?', default=False)
 
     def __str__(self):
         return f'{self.name} {self.surname}, контакты - {self.completed}, город - {self.city}, ' \
-               f'дата - {self.exhibition_date}, место - {self.exhibition_place}'
+               f'дата - {self.date}, место - {self.place}'
 
 
 class QuizzesForStudentsApplication(models.Model):
@@ -41,8 +41,8 @@ class QuizzesForStudentsApplication(models.Model):
     """ Нужно добавить строку с 'другое' с возможностью ввода своего варианта. """
 
     QUIZ_TYPES = [
-        ('Малая', 'Малая викторина 45 мин. на класс'),
-        ('Большая', 'Большая викторина 1,5 ч. на 50-100 детей'),
+        ('Малая викторина 45 мин. на класс', 'Малая викторина 45 мин. на класс'),
+        ('Большая викторина 1,5 ч. на 50-100 детей', 'Большая викторина 1,5 ч. на 50-100 детей'),
         ('Оба варианта', 'Оба варианта'),
     ]
 
@@ -50,7 +50,7 @@ class QuizzesForStudentsApplication(models.Model):
     surname = models.CharField(verbose_name='Фамилия', max_length=50)
     city = models.CharField(verbose_name='Город', max_length=50)
     contacts = models.CharField(verbose_name='ВК или email', max_length=100)
-    who_is_organize = models.CharField(verbose_name='Кто будет организатором выстаки?',
+    who_is_organize = models.CharField(verbose_name='Кто будет организатором выставки?',
                                        max_length=100,
                                        choices=ORGANIZER,
                                        default='Лично')
@@ -58,10 +58,10 @@ class QuizzesForStudentsApplication(models.Model):
                                    max_length=50,
                                    choices=QUIZ_TYPES,
                                    default='Малая')
-    exhibition_date = models.DateField(verbose_name='Когда примерно хотите организовывать выставку?')
-    exhibition_place = models.CharField(verbose_name='Где хотите провести выставку?', max_length=100)
+    date = models.DateField(verbose_name='Когда примерно хотите организовывать выставку?')
+    place = models.CharField(verbose_name='Где хотите провести выставку?', max_length=100)
     completed = models.BooleanField(verbose_name='Проведена ли ворина?', default=False)
 
     def __str__(self):
         return f'{self.name} {self.surname}, контакты - {self.completed}, город - {self.city}, ' \
-               f'дата - {self.exhibition_date}, место - {self.exhibition_place}'
+               f'дата - {self.date}, место - {self.place}'
