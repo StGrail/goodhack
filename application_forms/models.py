b93d1775d83ba685add46fe36f015f0be3abbd74
G <zzmrlol@gmail.com>
2021-04-23 12:55:02 +0300

    Add verbose name for admin panel

--- application_forms/models.py
+++ application_forms/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 
-class ExhibitionApplication(models.Model):
+class Event(models.Model):
+    """
+    Базовый класс для заявок на выставки и школьные викторины.
+    Включает в себя полностью рабочий класс для выставки.
+    """
+
     ORGANIZER = [
         ('Лично', 'Вы лично (активист)'),
         ('Волонтерское движение', 'Волонтерское движение'),
@@ -10,12 +15,10 @@ class ExhibitionApplication(models.Model):
         ('Молодежная организация', 'Молодежная организация'),
         ('Администрация города/района', 'Администрация города/района')
     ]
-    """ Нужно добавить строку с 'другое' с возможностью ввода своего варианта. """
-
     name = models.CharField(verbose_name='Имя', max_length=25)
     surname = models.CharField(verbose_name='Фамилия', max_length=50)
     city = models.CharField(verbose_name='Город', max_length=50)
-    contacts = models.CharField(verbose_name='ВК или email', max_length=100)
+    contacts = models.CharField(verbose_name='Контакты', max_length=100)
     who_is_organize = models.CharField(verbose_name='Кто будет организатором выставки?',
                                        max_length=100,
                                        choices=ORGANIZER,
@@ -25,43 +28,28 @@ class ExhibitionApplication(models.Model):
     completed = models.BooleanField(verbose_name='Проведена ли выставка?', default=False)
 
     def __str__(self):
-        return f'{self.name} {self.surname}, контакты - {self.completed}, город - {self.city}, ' \
-               f'дата - {self.date}, место - {self.place}'
+        return f'{self.name} {self.surname}, контакты - {self.contacts}, город - {self.city}, ' \
+               f'место проведения - {self.place}, дата проведения - {self.date}'
 
 
-class QuizzesForStudentsApplication(models.Model):
-    ORGANIZER = [
-        ('Лично', 'Вы лично (активист)'),
-        ('Волонтерское движение', 'Волонтерское движение'),
-        ('НКО', 'НКО'),
-        ('Образовательное учреждение', 'Образовательное учреждение'),
-        ('Молодежная организация', 'Молодежная организация'),
-        ('Администрация города/района', 'Администрация города/района'),
-    ]
-    """ Нужно добавить строку с 'другое' с возможностью ввода своего варианта. """
+class ExhibitionApplication(Event):
+    class Meta:
+        verbose_name_plural = 'Заявки на проведение выставок'
+        verbose_name = 'заявку на проведение выставки'
 
+
+class QuizzesForStudentsApplication(Event):
     QUIZ_TYPES = [
         ('Малая викторина 45 мин. на класс', 'Малая викторина 45 мин. на класс'),
         ('Большая викторина 1,5 ч. на 50-100 детей', 'Большая викторина 1,5 ч. на 50-100 детей'),
         ('Оба варианта', 'Оба варианта'),
     ]
 
-    name = models.CharField(verbose_name='Имя', max_length=25)
-    surname = models.CharField(verbose_name='Фамилия', max_length=50)
-    city = models.CharField(verbose_name='Город', max_length=50)
-    contacts = models.CharField(verbose_name='ВК или email', max_length=100)
-    who_is_organize = models.CharField(verbose_name='Кто будет организатором выставки?',
-                                       max_length=100,
-                                       choices=ORGANIZER,
-                                       default='Лично')
     quiz_format = models.CharField(verbose_name='Какой формат викторин интересует?',
                                    max_length=50,
                                    choices=QUIZ_TYPES,
                                    default='Малая')
-    date = models.DateField(verbose_name='Когда примерно хотите организовывать выставку?')
-    place = models.CharField(verbose_name='Где хотите провести выставку?', max_length=100)
-    completed = models.BooleanField(verbose_name='Проведена ли ворина?', default=False)
 
-    def __str__(self):
-        return f'{self.name} {self.surname}, контакты - {self.completed}, город - {self.city}, ' \
-               f'дата - {self.date}, место - {self.place}'
+    class Meta:
+        verbose_name_plural = 'заявки на проведение викторин для школьников'
+        verbose_name = 'заявку на проведение викторины для школьников'
