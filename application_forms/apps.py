78ce42a9446a96fae9d2c159b6374a97a5a13d7f
G <zzmrlol@gmail.com>
2021-04-23 16:24:18 +0300

    Add plural name for admin panel

--- application_forms/apps.py
+++ application_forms/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ApplicationFormConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'application_forms'
+    verbose_name = 'Заявки на проведение выставок и викторин'
