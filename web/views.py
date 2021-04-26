8d8c39735b2fab57b920f2aa27a00524908494d6
StGrail <zzmrlol@gmail.com>
2021-04-26 15:12:24 +0300

    Add about page

--- web/views.py
+++ web/views.py
@@ -4,3 +4,7 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "web/index.html"
 
+
+class AboutView(TemplateView):
+    template_name = "web/about.html"
+
