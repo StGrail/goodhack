d851c8d64f0192ec48eab37e520d2a1654cfeb3d
StGrail <zzmrlol@gmail.com>
2021-04-29 19:20:06 +0300

    Create dir tempaltes and move html tempaltes to it

--- core/settings.py
+++ core/settings.py
@@ -49,11 +49,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'core.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'media/',
+            TEMPLATES_DIR,
         ],
         'APP_DIRS': True,
         'OPTIONS': {
