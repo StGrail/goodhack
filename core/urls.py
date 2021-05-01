e0026fcb77d0150c2d63bf8163edf91b5be461e1
StGrail <zzmrlol@gmail.com>
2021-05-01 21:47:28 +0300

    Delete favico url

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
-from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
     path('', include('application_forms.urls')),
     path('', include('reports.urls')),
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
-    ),
 ]
