f1f9105e6f3e7fa0eaa52a2f90fb96498660e1b1
StGrail <zzmrlol@gmail.com>
2021-05-24 13:54:37 +0300

    Add conclusion for table

--- web/views.py
+++ web/views.py
@@ -1,8 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from application_forms.models import ExhibitionApplication, QuizzesForStudentsApplication
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "web/index.html"
+
+    def get_queryset(self):
+        exhibitions = ExhibitionApplication.objects.filter(completed=True).values().count()
+        quizzes = QuizzesForStudentsApplication.objects.filter(completed=True).values().count()
+        return exhibitions, quizzes
 
 
 class AboutView(TemplateView):
